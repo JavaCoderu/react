f8de9b72f7db4ae23517693f8d75055c78e22a78
VladislavBykov <Vladislav.Bykov02@mail.ru>
2020-02-23 05:49:48 +0700

    Posts in blog page and categories of posts

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -1,79 +1,91 @@
 import React, {Component} from 'react'
 import CarouselBox from "../Components/CarouselBox";
 import {Container, Row, Col, Media, Card, ListGroup} from "react-bootstrap"
+import {forEach} from "react-bootstrap/cjs/ElementChildren";
+
+let testData = [
+    {
+        blogImage : [150, 150, "mr-3", "https://blog.hyegyeong.com/assets/images/javascript.jpeg"],
+        title: "Blog post 1",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel nunc dui. Duis\n" +
+            "egestas, neque ut porta sodales, velit neque efficitur lectus, id feugiat felis\n" +
+            "magna ac sem. Curabitur nec."
+    },
+
+    {
+        blogImage : [150, 150, "mr-3", "https://blog.hyegyeong.com/assets/images/javascript.jpeg"],
+        title: "Blog post 2",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel nunc dui. Duis\n" +
+            "egestas, neque ut porta sodales, velit neque efficitur lectus, id feugiat felis\n" +
+            "magna ac sem. Curabitur nec."
+    },
+
+    {
+        blogImage : [150, 150, "mr-3", "https://blog.hyegyeong.com/assets/images/javascript.jpeg"],
+        title: "Blog post 3",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel nunc dui. Duis\n" +
+            "egestas, neque ut porta sodales, velit neque efficitur lectus, id feugiat felis\n" +
+            "magna ac sem. Curabitur nec."
+    },
+
+    {
+        blogImage : [150, 150, "mr-3", "https://blog.hyegyeong.com/assets/images/javascript.jpeg"],
+        title: "Blog post 4",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel nunc dui. Duis\n" +
+            "egestas, neque ut porta sodales, velit neque efficitur lectus, id feugiat felis\n" +
+            "magna ac sem. Curabitur nec."
+    },
+
+    {
+        blogImage : [150, 150, "mr-3", "https://blog.hyegyeong.com/assets/images/javascript.jpeg"],
+        title: "Blog post 5",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel nunc dui. Duis\n" +
+            "egestas, neque ut porta sodales, velit neque efficitur lectus, id feugiat felis\n" +
+            "magna ac sem. Curabitur nec."
+    },
+
+];
+
+let categories = ["Js", "React", "Meteor", "Java(ховно)"];
 
 export default class Blog extends Component {
+
     render() {
+
+            let list = testData.map((post)=>{
+                return (
+                    <Media className="m-5">
+                        <img
+                            width={post.blogImage[0]}
+                            hight={post.blogImage[1]}
+                            className={post.blogImage[2]}
+                            src={post.blogImage[3]}
+                        />
+                        <Media.Body>
+                            <h5>{post.titile}</h5>
+                            <p>{post.text}</p>
+                        </Media.Body>
+                    </Media>
+                )
+            });
+
+            let categ = categories.map((cat)=>{
+                return <ListGroup.Item>{cat}</ListGroup.Item>
+            })
+
         return (
             <Container>
                 <Row>
                     <Col md="9">
-                        <Media className="m-5">
-                            <img
-                                width={150}
-                                hight={150}
-                                className="mr-3"
-                                src="https://blog.hyegyeong.com/assets/images/javascript.jpeg"
-                            />
-                            <Media.Body>
-                                <h5>Blog post</h5>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel nunc dui. Duis
-                                    egestas, neque ut porta sodales, velit neque efficitur lectus, id feugiat felis
-                                    magna ac sem. Curabitur nec.</p>
-                            </Media.Body>
-                        </Media>
-                        <Media className="m-5">
-                            <img
-                                width={150}
-                                hight={150}
-                                className="mr-3"
-                                src="https://blog.hyegyeong.com/assets/images/javascript.jpeg"
-                            />
-                            <Media.Body>
-                                <h5>Blog post</h5>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel nunc dui. Duis
-                                    egestas, neque ut porta sodales, velit neque efficitur lectus, id feugiat felis
-                                    magna ac sem. Curabitur nec.</p>
-                            </Media.Body>
-                        </Media>
-                        <Media className="m-5">
-                            <img
-                                width={150}
-                                hight={150}
-                                className="mr-3"
-                                src="https://blog.hyegyeong.com/assets/images/javascript.jpeg"
-                            />
-                            <Media.Body>
-                                <h5>Blog post</h5>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel nunc dui. Duis
-                                    egestas, neque ut porta sodales, velit neque efficitur lectus, id feugiat felis
-                                    magna ac sem. Curabitur nec.</p>
-                            </Media.Body>
-                        </Media>
-                        <Media className="m-5">
-                            <img
-                                width={150}
-                                hight={150}
-                                className="mr-3"
-                                src="https://blog.hyegyeong.com/assets/images/javascript.jpeg"
-                            />
-                            <Media.Body>
-                                <h5>Blog post</h5>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel nunc dui. Duis
-                                    egestas, neque ut porta sodales, velit neque efficitur lectus, id feugiat felis
-                                    magna ac sem. Curabitur nec.</p>
-                            </Media.Body>
 
-                        </Media>
+                        {list}
+
                     </Col>
                     <Col md="3">
                         <h5 className="text-center mt-4">Categories</h5>
                         <Card>
                             <ListGroup variant="flush">
-                                <ListGroup.Item>JS</ListGroup.Item>
-                                <ListGroup.Item>React</ListGroup.Item>
-                                <ListGroup.Item>Meteor</ListGroup.Item>
-                                <ListGroup.Item>Java</ListGroup.Item>
+                                {categ}
                             </ListGroup>
                         </Card>
 
